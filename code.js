7b2bc8cf4cad92ff12856828750c756560826920
sophshap <sophieashapiro@gmail.com>
2023-11-06 12:10:15 -0500

    completed workshop

---
+++ code.js
@@ -0,0 +1,21 @@
+// Create a string for the user as an alert/popup
+// Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/). Each must equal one of the three codes in the combination.
+// Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+
+
+// create greeting
+const greeting = ("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+
+// alert below
+alert(greeting)
+
+// assign variables
+const var1 = 5 + 5
+const var2 = 5 * 5
+const var3 = 5 - 5
+
+// alert with combo
+alert(`${var1} ${var2} ${var3}`)
+
+// extra alert for fun
+alert("Use this code to unlock the vault!")
